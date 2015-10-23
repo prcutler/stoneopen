0bcbf8ec070ad69c421922dc720c094f7bded103
Paul Cutler <paul.r.cutler@gmail.com>
2015-10-23 07:20:20 -0500

    Remove all files
    Delete all files in preparation for a new re-design on stoneopen2 branch

--- wp-content/themes/guesthouse/AIT/Framework/Shortcodes/boxesFrames/load.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-require_once "ait-boxes.php";
-require_once "ait-box-title.php";
--- wp-content/themes/guesthouse/AIT/Framework/Shortcodes/social/load.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once "ait-facebook-box.php";
-require_once "ait-facebook-comments.php";
-require_once "ait-twitter.php";
