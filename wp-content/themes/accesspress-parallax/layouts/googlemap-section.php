b0f652eab5d9aff72aa674c7fca84b01e87512ba
Paul Cutler <paul.r.cutler@gmail.com>
2015-10-30 09:14:59 -0400

    Update themes

---
+++ wp-content/themes/accesspress-parallax/layouts/googlemap-section.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying all Parallax Templates.
+ *
+ * @package accesspress_parallax
+ */
+?>
+
+	<div class="content-area googlemap-section">
+	<div class="googlemap-toggle"><?php _e('Map','accesspress-parallax'); ?></div>
+	<div class="googlemap-content">
+		<?php echo $page->post_content; ?>
+	</div>
+	</div><!-- #primary -->
+
+
+
